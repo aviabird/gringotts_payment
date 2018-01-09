bf2b3be47127538c39a6818e48c60eae5a2528d9
Gopal Shimpi <gopalshimpi@gmail.com>
2018-01-09 14:52:15 +0530

    Implmented purchase method

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,6 +11,7 @@
 //
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
+
 import "phoenix_html"
 import {sidebar, sidebarCheck} from "./sidebar.js";
 // Import local files
