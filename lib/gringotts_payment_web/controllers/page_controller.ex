683334175e17389a9766571aa750f613e68f2ba5
Arjun Singh <arjun289@users.noreply.github.com>
2018-01-11 18:59:51 +0530

    Stripe integration (#2) (#4)
    * stripe integration
    
    added the basic skeleton for integration
    with gringotts stripe payment gateway.
    
    * modified frontend
    
    Moidified the frontend. Changed page controller
    to render json response. Made changes to handle the
    json response in js file.
    
    * change js file for response type
    
    Made changes to js file to show a successful response
    or error depending on the response type from the server.
    
    * modify url
    
    * production config for heroku
    
    * added github dev as lib for gringotts
    
    * enable inputs after error
    
    changed app js to enable form inputs
    after error.
    
    * Add Sidebar
    
    Added sidebar to accomodate list of gateways.
    
    * style changes
    
    * modified readme
    
    * changes for page specific js

--- lib/gringotts_payment_web/controllers/page_controller.ex
+++ lib/gringotts_payment_web/controllers/page_controller.ex
@@ -1,7 +1,21 @@
 defmodule GringottsPaymentWeb.PageController do
   use GringottsPaymentWeb, :controller
+  alias Gringotts, as: Billing
+  alias Gringotts.Gateways.Stripe
+
+  @options [currency: "usd"]
 
   def index(conn, _params) do
     render conn, "index.html"
+  end
+
+  def purchase(conn, %{"id" => id }) do
+    value = Billing.purchase(Stripe, 5, id, @options)
+    cond do
+      value["created"] ->
+        render conn, "purchase.json", success: value
+      value["error"] -> 
+        render conn, "purchase.json", error: value
+    end
   end
 end
--- lib/gringotts_payment_web/router.ex
+++ lib/gringotts_payment_web/router.ex
@@ -5,7 +5,7 @@ defmodule GringottsPaymentWeb.Router do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_flash
-    plug :protect_from_forgery
+    # plug :protect_from_forgery
     plug :put_secure_browser_headers
   end
 
@@ -17,6 +17,13 @@ defmodule GringottsPaymentWeb.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+  end
+
+  scope "/api", GringottsPaymentWeb do
+    pipe_through :api # Use the default browser stack
+
+    post "/purchase", PageController, :purchase
+    options "/purchase", PageController, :options
   end
 
   # Other scopes may use custom stacks.
